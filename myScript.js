348c53d919b605281de2adb1cfd2de2ea6693ac0
indergrewal <indergrewal1208.gi@gmail.com>
2023-03-30 12:02:11 -0400

    updated

--- myScript.js
+++ myScript.js
@@ -1,42 +1,17 @@
-value= document.getElementById("tx");
-    value.innerHTML= "Inderjit Singh";
-    const screenA = document.getElementById('A');
-const screenB = document.getElementById('B');
+const elements = document.querySelectorAll('.bounce');
 
-
-var isScrolling= false;
-// document.addEventListener('wheel', function(event){
-//     if (event.deltaY >0 && !isScrolling) {
-//         isScrolling = true;
-//         screenB.scrollIntoView({ behavior: 'smooth', setInterval: '500' });
-//         console.log("Scrolling");
-//         event.preventDefault();
-//         setTimeout(function() {
-//             isScrolling = false;
-//         }, 500);
-//     }
-//     else  if (event.deltaY <0 && !isScrolling) {
-//         isScrolling = true;
-//         screenA.scrollIntoView({ behavior: 'smooth' , setInterval: '500'  });
-//         console.log("Scrolling");
-//         event.preventDefault();
-//         setTimeout(function() {
-//             isScrolling = false;
-//         }, 500);
-
-//     }
-// }, { passive: false });
-const container = document.querySelector('.container');
-const namePics = document.querySelectorAll('.NamePic');
-
-container.addEventListener('scroll', () => {
-  const scrollTop = container.scrollTop;
-
-  namePics.forEach((namePic) => {
-    if (namePic.offsetTop <= scrollTop && namePic.offsetTop + namePic.offsetHeight > scrollTop) {
-      console.log('Currently snapped:', namePic.id);
-      console.log('offset: '+namePic.offsetTop+ ' scrollTop ' + scrollTop+ ' offsetHeight: '+namePic.offsetHeight);
-    }
-  });
+elements.forEach(element => {
+  element.addEventListener('mouseenter', () => {
+    console.log('Mouse over element with class "bounce"');
+animationCall(element)  });
 });
 
+
+function animationCall(t){
+
+t.classList.add('hover');
+console.log('class :'+ t.className);
+setTimeout(function(){
+  t.classList.remove('hover');
+}, 1010)
+}
