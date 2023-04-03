d0b70d62fa60e7255be27ec764c8d513779761c6
indergrewal <indergrewal1208.gi@gmail.com>
2023-04-03 16:41:36 -0400

    improved

--- myScript.js
+++ myScript.js
@@ -1,16 +1,53 @@
 const elements = document.querySelectorAll('.bounce');
+const sideButtons =Array.from(document.querySelectorAll('.sideButton'));
+const sideLinks = Array.from(document.querySelectorAll('.sideLink'));
+const home = document.querySelector('.homeButton');
+console.log("Number of sideButtons: " + sideButtons.length);
+
+
+home.addEventListener('click', function() {
+  window.location.href = 'index.html';
+});
 
 elements.forEach(element => {
   element.addEventListener('mouseenter', () => {
-    console.log('Mouse over element with class "bounce"');
+
 animationCall(element)  });
 });
 
+sideButtons.forEach(button=>{
+  button.addEventListener('click', ()=>{
+    menuToggle(sideButtons.indexOf(button))});
+});
+
+
+function menuToggle(b)
+{
+  sideLinks.forEach(sideLink=>{
+    sideLink.style.display='none';
+  });
+  sideLinks[b+1].style.display="grid";
+  console.log("toggle called at button: "+sideLinks[b].className);
+}
+
+/*********/
+
+/*********/
+
+
+
+for(let i=0; i<elements.length;i++)
+{
+  //setTimeout(animationCall(elements[i]),500);
+  setTimeout(() => {
+    animationCall(elements[i]);
+  }, i*50);
+}
 
 function animationCall(t){
 
 t.classList.add('hover');
-console.log('class :'+ t.className);
+
 setTimeout(function(){
   t.classList.remove('hover');
 }, 1010)
